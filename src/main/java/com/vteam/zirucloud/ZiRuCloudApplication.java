7e2428ea9a6a20c69e3c636c3ced33b4bad398e4
Aoi-Sora-lan <2523627390@qq.com>
2023-05-25 23:02:09 +0800

    创建项目

---
+++ src/main/java/com/vteam/zirucloud/ZiRuCloudApplication.java
@@ -0,0 +1,13 @@
+package com.vteam.zirucloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiRuCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiRuCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vteam/zirucloud/ZiRuCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vteam.zirucloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiRuCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
